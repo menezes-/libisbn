2bbbfd123febcf0c4d69170cf3915cf3884f7095
Gabriel Menezes <gabrielmenezesvi@gmail.com>
2016-01-21 11:11:34 -0200

    Fix python installation Add dependencies on python code to trigger rebuild and reinstall

--- pylibisbn/py_package/setup.py
+++ pylibisbn/py_package/setup.py
@@ -1,14 +1,23 @@
 from setuptools import setup, find_packages
+import os
+
+def package_data():
+    if os.name == 'nt':
+        return ["*.pyd", "*.dll"]
+    else:
+        return ["*.so"]
 
 setup(
         name='pylibisbn',
         version='0.1',
         packages=find_packages(exclude=['tests']),
         url='https://github.com/menezes-/libisbn',
-        license='',
+        package_data={"pylibisbn":package_data()},
+        license='MIT',
         author='Gabriel',
         author_email='',
         description='Simple and Fast ISBN library written in C++',
         test_suite='tests',
+        zip_safe=False
 
 )
